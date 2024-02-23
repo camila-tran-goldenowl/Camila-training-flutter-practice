7c45b6929f4dc2522769dc5c2b8b22181a3e6bab
camila.tran.goldenowl <camila.tran.goldenowl@gmail.com>
2024-02-23 16:58:54 +0700

    revert version

--- todo/lib/src/features/home/widgets/todo_list.dart
+++ todo/lib/src/features/home/widgets/todo_list.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:myapp/widgets/todo/card.dart';
 
 class TodoList extends StatelessWidget {
-
-  const TodoList({super.key});
+  const TodoList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      TodoCard(task: 'Task 1', completed: false, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-      TodoCard(task: 'Task 2', completed: true, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-      TodoCard(task: 'Task 3', completed: true, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-      TodoCard(task: 'Task 4', completed: false, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-      TodoCard(task: 'Task 5', completed: false, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-      TodoCard(task: 'Task 6', completed: false, onDelete: (){}, onEdit:(){}),
-      const SizedBox(height: 8.0),
-    ]);
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          TodoCard(task: 'Task 1', completed: false, onDelete: () {}, onEdit: () {}),
+          const SizedBox(height: 8.0),
+          TodoCard(task: 'Task 2', completed: true, onDelete: () {}, onEdit: () {}),
+          const SizedBox(height: 8.0),
+          TodoCard(task: 'Task 3', completed: true, onDelete: () {}, onEdit: () {}),
+          const SizedBox(height: 8.0),
+          TodoCard(task: 'Task 4', completed: false, onDelete: () {}, onEdit: () {}),
+          const SizedBox(height: 8.0),
+          TodoCard(task: 'Task 5', completed: false, onDelete: () {}, onEdit: () {}),
+          const SizedBox(height: 8.0),
+          TodoCard(task: 'Task 6', completed: false, onDelete: () {}, onEdit: () {}),
+        ],
+      ),
+    );
   }
 }
